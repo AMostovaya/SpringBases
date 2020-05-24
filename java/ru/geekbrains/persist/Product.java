a512629a26ce0ac2e28cf446a3b23723c3c0e213
Alexandra Mostovaya <31797807+AMostovaya@users.noreply.github.com>
2020-05-25 03:29:49 +0600

    Add files via upload

---
+++ java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,78 @@
+package ru.geekbrains.persist;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cost")
+    private Integer cost;
+
+    @ManyToMany
+    @JoinTable(
+            name = "clients_products",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "client_id")
+    )
+    private List<Client> clients;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, Integer cost) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
