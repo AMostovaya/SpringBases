a52df4f93261be183c76018f2287e736ecab1e36
Alexandra Mostovaya <31797807+AMostovaya@users.noreply.github.com>
2020-06-01 19:15:03 +0600

    Update UserController.java
    // добавлен переход на список

--- UserController.java
+++ UserController.java
@@ -86,7 +86,7 @@ public class UserController {
 
         userService.save(user);
         model.addAttribute("users", userService.findAll());
-        return "users";
+        return "redirect:/user";
     }
 
 
